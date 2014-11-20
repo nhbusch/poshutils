4092d2914c4b6552c2539386700659e215a94589
Nils Busch <nh.busch@web.de>
2014-11-20 19:35:37 +0100

    Fix module import.
    Simplify module import.
    Correct name in license.
    Move posh-git module to correct path.
    
    :set textwidth=72

--- profile.ps1
+++ profile.ps1
@@ -4,20 +4,57 @@
 #
 # Copyright (c) 2014 Nils H. Busch. All rights reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0.txt
+# Licensed under the MIT License (MIT).
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# You should have received a copy of the MIT License along with this file.
+# If not, you may obtain a copy at http://opensource.org/licenses/MIT.
 #
 ###########################################################################
 
-# Add paths
+$SourcePath = Join-Path ([Environment]::GetFolderPath('MyDocuments')) 'WindowsPowerShell'
+
+###########################################################################
+# Import modules
+$UserModulePath = Join-Path -Path ($SourcePath) -ChildPath 'Modules'
+
+Import-Module -Name (Join-Path (Join-Path $UserModulePath 'PsGet') 'PsGet')
+Import-Module posh-git
+
+###########################################################################
+# Functions
+
+# Sets up a simple prompt, adding the git prompt parts inside git repos
+function global:prompt 
+{
+  $realLASTEXITCODE = $LASTEXITCODE
+
+  # Reset color, which can be messed up by Enable-GitColors
+  $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+  # Replace home path with tilde
+  Write-Host($pwd.ProviderPath.replace("$env:USERPROFILE",'~')) -ForegroundColor DarkYellow
+
+  Write-VcsStatus
+
+  $global:LASTEXITCODE = $realLASTEXITCODE
+  Write-Host "$env:USERNAME@$env:COMPUTERNAME$('>' * ($nestedPromptLevel + 1))" -ForegroundColor Blue -NoNewline
+  return " "
+}
+
+# Fancier output for Git
+Enable-GitColors
+
+#if(Test-Path -Path (Join-Path $ExpectedUserModulePath 'PsGet') -PathType Container)
+#{
+#  Import-Module -Name (Join-Path $ExpectedUserModulePath 'PsGet')
+#}
+
+#if(Test-Path -Path (Join-Path $ExpectedUserModulePath 'PoshGit') -PathType Container)
+#{
+#  Import-Module -Name (Join-Path $ExpectedUserModulePath 'posh-git')
+#}
+
+# Adds path to utilities to PATH environment variable
 function Add-UtilsPath
 {
   $tail = Join-Path 'opt' 'utils'
@@ -30,9 +67,10 @@ function Add-UtilsPath
     }
   }
 }
-Add-UtilsPath 
+Add-UtilsPath
 
-# Opens Windows PowerShell Help as a compiled HTML Help file (.chm). 
+
+# Opens Windows PowerShell Help as a compiled HTML Help file (.chm).
 function Get-CHM
 {
   (Invoke-Item $env:windir\help\mui\0409\WindowsPowerShellHelp.chm)
@@ -51,30 +89,22 @@ function Get-CmdletAlias($cmdletname)
      Definition, Name -auto
 }
 
-$SourcePath = Join-Path ([Environment]::GetFolderPath('MyDocuments')) 'WindowsPowerShell'
-
-# Import modules
-$ExpectedUserModulePath = Join-Path -Path ($SourcePath) -ChildPath 'Modules'
-if(Test-Path -Path (Join-Path $ExpectedUserModulePath 'PsGet') -PathType Container)
-{
-  Import-Module -Name (Join-Path $ExpectedUserModulePath 'PsGet')
-}
-
+###########################################################################
 # Source scripts
 $user_cmds = 'Add-Signature','Add-Path','Invoke-WalkCommand','VisualStudioUtils'
-$user_cmds | %{ 
-  if(Test-Path -Path (Join-Path $SourcePath ($_ + '.ps1')) -PathType Leaf)
-  {
-	  . (Join-Path $SourcePath ($_ + '.ps1'))
+$user_cmds | %{
+  if(Test-Path -Path (Join-Path $SourcePath ($_ + '.ps1')) -PathType Leaf) {
+    . (Join-Path $SourcePath ($_ + '.ps1'))
   }
 }
 
-# Load posh-git profile
-if(Test-Path -Path (Join-Path $ExpectedUserModulePath 'PoshGit\profile.ps1') -PathType Leaf)
-{
-  . (Join-Path $ExpectedUserModulePath 'PoshGit\profile.ps1')
-}
+## Load posh-git profile
+#if(Test-Path -Path (Join-Path $ExpectedUserModulePath 'PoshGit\profile.ps1') -PathType Leaf)
+#{
+#  . (Join-Path $ExpectedUserModulePath 'PoshGit\profile.ps1')
+#}
 
+###########################################################################
 # PS drives
 New-PSDrive -Name user -PSProvider filesystem -Root $env:USERPROFILE | Out-Null
 New-PSDrive -Name dev -PSProvider filesystem -Root $env:USERPROFILE\Dev | Out-Null
@@ -82,6 +112,7 @@ New-PSDrive -Name scripts -PSProvider filesystem -Root $SourcePath | Out-Null
 New-PSDrive -Name build -PSProvider filesystem -Root C:\build | Out-Null
 New-PSDrive -Name repos -PSProvider filesystem -Root $env:USERPROFILE\Dev\repos | Out-Null
 
+###########################################################################
 # Aliases
 New-Alias -Name ivv -Value Import-VisualStudioVars
 New-Alias -Name ass -Value Start-SshAgent 
@@ -90,5 +121,5 @@ New-Alias -Name walk -Value Invoke-WalkCommand
 
 # Change to powershell source directory
 if (Test-Path -Path $SourcePath -PathType Container) {
-  Set-Location $SourcePath 
+  Set-Location $SourcePath
 }
